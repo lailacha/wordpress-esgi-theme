45d8340a9ae47a6f1a34a1894f71456e74164353
Ossama9 <oussama.dahbi98@gmail.com>
2022-07-21 23:23:05 +0200

    Add list partners

---
+++ template-parts/list-partners.php
@@ -0,0 +1,42 @@
+<h2><?php echo get_theme_mod( 'titre_partners', 'Our Partners' ) ?></h2>
+    <div class="col-12 list-partners mt-5">
+
+
+        <div class="col-2">
+            <img class="image-card"
+                 src="<?php echo get_theme_mod( 'add partner 1', get_bloginfo( 'template_url' ) . '/assets/images/partner-1.svg' ) ?>"
+                 alt="">
+        </div>
+        <div class="col-2">
+            <img class="image-card"
+                 src="<?php echo get_theme_mod( 'add partner 2', get_bloginfo( 'template_url' ) . '/assets/images/partner-2.svg' ) ?>"
+                 alt="">
+        </div>
+
+        <div class="col-2">
+            <img  class="image-card"
+                 src="<?php echo get_theme_mod( 'add partner 3', get_bloginfo( 'template_url' ) . '/assets/images/partner-3.svg' ) ?>"
+                 alt="">
+        </div>
+
+        <div class="col-2">
+            <img class="image-card"
+                 src="<?php echo get_theme_mod( 'add partner 4', get_bloginfo( 'template_url' ) . '/assets/images/partner-4.svg' ) ?>"
+                 alt="">
+        </div>
+
+
+        <div class="col-2">
+            <img class="image-card"
+                 src="<?php echo get_theme_mod( 'add partner 5', get_bloginfo( 'template_url' ) . '/assets/images/partner-5.svg' ) ?>"
+                 alt="">
+        </div>
+
+        <div class="col-2">
+            <img class="image-card"
+                 src="<?php echo get_theme_mod( 'add partner 6', get_bloginfo( 'template_url' ) . '/assets/images/partner-6.svg' ) ?>"
+                 alt="">
+        </div>
+
+
+</div>
---
+++ templates/our_partners.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  Template Name: Our Partners
+*/
+
+?>
+<?php get_header(); ?>
+    <main>
+        <div class="container">
+            <div class="row" id="blog">
+                <?php get_template_part("template-parts/list-partners"); ?>
+
+            </div>
+        </div>
+
+    </main>
+<?php get_footer(); ?>
