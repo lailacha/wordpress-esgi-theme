47c05dee355a91ad7030ab24fe9014f91997afda
LailaCharaoui <mats2@live.fr>
2022-07-22 01:02:17 +0200

    fix(post-list): add parameter to category class

--- template-parts/post-list.php
+++ template-parts/post-list.php
@@ -7,15 +7,13 @@ if ( $the_query->have_posts() ) {
         <div class="post-item">
             <div class="post-item-image"
                  style="background-image: url('<?php echo get_the_post_thumbnail_url( $page->ID ) ?? '/uploads/2022/03/cropped-screenshot.png' ?>')">
-                <span class="post-item-category"><?= get_category( $page->ID ) ?? "Uncategorized " ?></span>
+                <span class="post-item-category"><?= get_category( $page->ID )->name ?? "Uncategorized " ?></span>
             </div>
             <div class="post-item-content">
                 <div class="post-item-title">
                     <a href="<?= get_permalink( $page->ID ) ?>"><?= $page->post_title ?></a>
                     <p class="post-item-text"><?= substr( strip_tags( $page->post_content ), 0, 120 ); ?></p>
                 </div>
-
-
             </div>
         </div>
 	<?php } ?>
