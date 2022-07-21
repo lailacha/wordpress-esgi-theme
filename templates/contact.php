386eb1d6bb22b49525dbc14f3df580ff0397a50d
Ossama9 <oussama.dahbi98@gmail.com>
2022-07-21 19:40:50 +0200

    init branch

---
+++ templates/contact.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  Template Name: Contact
+*/
+?>
+<?php get_header(); ?>
+    <main>
+        <div class="container-fluid page-contact ps-5">
+            <div class="row">
+                <h2 class="contact-title"><?= ucfirst($post->post_title) ?>.</h2>
+                <h5 class="contact-subtitle">A desire for a big party or a very select congress? Contact us</h5>
+            </div>
+
+
+        </div>
+
+    </main>
+<?php get_footer(); ?>
