f3d6b41edb597281b83ae0ccaf4fbddb9a36edf0
LailaCharaoui <mats2@live.fr>
2022-07-21 16:18:49 +0200

    fix(menu)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4040,59 +4040,10 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
     return C.$ === S && (C.$ = Gt), e && C.jQuery === S && (C.jQuery = Vt), S;
   }, "undefined" == typeof e && (C.jQuery = C.$ = S), S;
 });
-/*
-$(document).ready(function(){
-
-	ajaxizePageLinks();
-
-
-	//toogle element when click on it
-	$('.toggle-item').click(function(){
-		$('.toggle-item').toggleClass('close');
-		$('.burger-menu').toggleClass('burger-open');
-	}
-	);
-
-})
-
-function showPage(page){
-	console.log(page)
-	$.ajax({
-		url: esgi.ajaxURL,
-		type: 'POST',
-		data: {
-			'action': 'load_posts',
-			'page' : page
-		}
-	}).done(function(reponse){
-		$('#post-list-wrapper').html(reponse);
-	})
-}
-
-
-function ajaxizePageLinks(){
-	var page = 1;
-	$('.page-numbers').click(function(e){
-		e.preventDefault();
-
-		var currentPage = $('.page-numbers.current').html();
-		if($(this).hasClass('next')){
-			page = Number(currentPage) + 1;
-		}
-		else if($(this).hasClass('prev')){
-			page = Number(currentPage) - 1;
-		}
-		else{
-			page = $(this).html();
-		}
-		showPage(page);
-
-
-		const nextState = {};
-		const nextTitle = 'Page - ' + page;
-		const nextURL = $(this).attr('href');
-		window.history.replaceState(nextState, nextTitle, nextURL);
-
-	})
-}
-*/
+$(document).ready(function () {
+  //toogle element when click on it
+  $('.toggle-item').click(function () {
+    $('.toggle-item').toggleClass('close');
+    $('.burger-menu').toggleClass('burger-open');
+  });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
-/*
+
 $(document).ready(function(){
-
-	ajaxizePageLinks();
-
 
 	//toogle element when click on it
 	$('.toggle-item').click(function(){
@@ -13,44 +10,5 @@ $(document).ready(function(){
 
 })
 
-function showPage(page){
-	console.log(page)
-	$.ajax({
-		url: esgi.ajaxURL,
-		type: 'POST',
-		data: {
-			'action': 'load_posts',
-			'page' : page
-		}
-	}).done(function(reponse){
-		$('#post-list-wrapper').html(reponse);
-	})
-}
 
 
-function ajaxizePageLinks(){
-	var page = 1;
-	$('.page-numbers').click(function(e){
-		e.preventDefault();
-
-		var currentPage = $('.page-numbers.current').html();
-		if($(this).hasClass('next')){
-			page = Number(currentPage) + 1;
-		}
-		else if($(this).hasClass('prev')){
-			page = Number(currentPage) - 1;
-		}
-		else{
-			page = $(this).html();
-		}
-		showPage(page);
-
-
-		const nextState = {};
-		const nextTitle = 'Page - ' + page;
-		const nextURL = $(this).attr('href');
-		window.history.replaceState(nextState, nextTitle, nextURL);
-
-	})
-}
-*/
